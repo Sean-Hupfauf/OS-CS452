913c796da3bcfcb972d4ace15c93bf1de1480393
Sean-Hupfauf <12baysidesmh@gmail.com>
2019-10-14 17:03:43 -0500

    TCP Connection between servers
    Follow the make file, nothing gets sent between currently.

---
+++ README.md
@@ -0,0 +1,13 @@
+# TCP_Sockets
+CS452 Assignment 2
+
+## Usage
+
+to compile:
+`make`
+
+Run Server on thing3:
+`./server`
+
+Run Client on thing0:
+`./client thing3`
---
+++ client.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <iostream>
+#include <bitset>
+#include <math.h>
+
+#define PORT 9554
+#define MAXVALUE 11500
+
+using namespace std;
+
+void error(const char *msg) {
+	perror(msg);
+	exit(0);
+}
+
+int main(int argc, char*argv[]) {
+
+	/*
+	============================
+	SET UP CONNECTION
+	============================
+	*/
+
+	int sockfd, n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	if (argc < 2) {
+		fprintf(stderr, "usage is %s hostname\n", argv[0]);
+		exit(0);
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) error("ERROR opening socket");
+		server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+		(char *)&serv_addr.sin_addr.s_addr,
+		server->h_length);
+	serv_addr.sin_port = htons(PORT);
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR connecting");
+	}
+
+	
+	close(sockfd);
+	return 0;
+}
